18a0e5aab5e0ff324068c72ba9748075a2829971
Victor Raony Moraes Lima <victorraony@MacBook-Pro-de-Victor.local>
2024-04-03 23:14:15 -0300

    Feat: Implementing FindByIdEvents

--- src/data/event_handler.py
+++ src/data/event_handler.py
@@ -17,3 +17,21 @@ class EventHandler:
         body={ "eventId" : body["uuid"]},
         status_code=200
       )
+  
+  def find_by_id(self, http_request: HttpRequest) -> HttpResponse:
+      event_id = http_request.param["event_id"]
+      event = self.__events_respositry.get_eventy_by_id(event_id)
+      if not event: raise Exception("Evento nao encontrado")
+      
+      return HttpResponse(
+          body={
+              "event": {
+                  "id": event.id,
+                  "title": event.title,
+                  "details": event.details,
+                  "slug": event.slug,
+                  "maximum_attendees": event.maximum_attendees
+                }
+          },
+          status_code=200
+      )   
